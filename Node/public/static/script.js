76b58851d447339cf0ef5f68ca7b6e2ed7594b0b
Javier Iturri <javier.iturri@biko2.com>
2022-01-26 12:52:49 +0100

    Logica guardar input

--- Node/public/static/script.js
+++ Node/public/static/script.js
@@ -1,13 +1,12 @@
-const axios = require('axios')
-
+let getTask = '';
 let task = '';
 function handleChangeTask() {
     var x = document.getElementById('task');
-    task = x.value
+    getTask = x.value
 }
 
 function handleClick(value) {
-    task = ''
-    task = { "task": value }
-    axios.post('http://127.0.0.1:3000/html/tasks', task).then(console.log(task))
-}
+    task = { "name": value }
+    console.log(task)
+    console.log(typeof(task))
+} 
